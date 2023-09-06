c7f05bb2e98b3ccffa375790926c1904304bf734
jenny <fernandez.jennyp@gmail.com>
2023-09-06 22:51:00 +0800

    commit(6)

--- DATABASE FILE/simpleloan.sql
+++ DATABASE FILE/simpleloan.sql
@@ -1,13 +1,14 @@
 -- phpMyAdmin SQL Dump
--- version 4.6.5.2
+-- version 5.2.0
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Aug 27, 2022 at 06:40 PM
--- Server version: 5.6.21
--- PHP Version: 5.6.3
+-- Generation Time: Sep 06, 2023 at 04:42 PM
+-- Server version: 10.4.24-MariaDB
+-- PHP Version: 8.1.6
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
 SET time_zone = "+00:00";
 
 
@@ -42,116 +43,90 @@ INSERT INTO `admin` (`admin_id`, `user`, `pass`) VALUES
 -- --------------------------------------------------------
 
 --
--- Table structure for table `category`
+-- Table structure for table `cash_collection_2020`
 --
 
-CREATE TABLE `category` (
-  `cat_id` int(11) NOT NULL,
-  `cat_name` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+CREATE TABLE `cash_collection_2020` (
+  `id` int(11) NOT NULL,
+  `date` date DEFAULT NULL,
+  `or_num` int(11) DEFAULT NULL,
+  `charged_colln` float DEFAULT NULL,
+  `charged_total` float DEFAULT NULL,
+  `d_sales_for` float DEFAULT NULL,
+  `amount` float DEFAULT NULL,
+  `overage` float DEFAULT NULL,
+  `total` float DEFAULT NULL,
+  `c_total` float DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data for table `category`
+-- Dumping data for table `cash_collection_2020`
 --
 
-INSERT INTO `category` (`cat_id`, `cat_name`) VALUES
-(300, 'Male'),
-(301, 'Female'),
-(302, 'Youth'),
-(303, 'All');
+INSERT INTO `cash_collection_2020` (`id`, `date`, `or_num`, `charged_colln`, `charged_total`, `d_sales_for`, `amount`, `overage`, `total`, `c_total`) VALUES
+(1, '2023-09-02', 321313, 0, 0, 0, 0, 0, 0, 0),
+(2, '2023-09-03', 54355, 0, 0, 0, 0, 0, 0, 0),
+(3, '2023-09-06', 321123313, 31431, 3213, 321321, 32131, 321421, 32131, 32313),
+(4, '2023-09-06', 312141, 3211230000, 32113, 3213420, 3213420, 323421, 0, 0);
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `district`
+-- Table structure for table `jenny_fernandez`
 --
 
-CREATE TABLE `district` (
-  `district_id` int(11) NOT NULL,
-  `district_name` varchar(255) NOT NULL,
-  `region_id` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+CREATE TABLE `jenny_fernandez` (
+  `id` int(11) NOT NULL,
+  `date` date DEFAULT NULL,
+  `loan_amount` float DEFAULT NULL,
+  `payment_amount` float DEFAULT NULL,
+  `or_num` int(11) DEFAULT NULL,
+  `or_date` date DEFAULT NULL,
+  `amount_balance` float DEFAULT NULL,
+  `loan_balance` float DEFAULT NULL,
+  `sc_starts` varchar(255) DEFAULT NULL,
+  `four_percent` float DEFAULT NULL,
+  `sc_dates` varchar(255) DEFAULT NULL,
+  `months` int(11) DEFAULT NULL,
+  `four_percent_sc` float DEFAULT NULL,
+  `sc_payments` float DEFAULT NULL,
+  `sc_payments_or_num` int(11) DEFAULT NULL,
+  `sc_payments_date` date DEFAULT NULL,
+  `sc_balance` float DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data for table `district`
+-- Dumping data for table `jenny_fernandez`
 --
 
-INSERT INTO `district` (`district_id`, `district_name`, `region_id`) VALUES
-(50, 'Itilima', 25),
-(51, 'Bariadi', 25),
-(52, 'Meatu', 25),
-(53, 'Busega', 25),
-(54, 'Maswa', 25),
-(56, 'Ucreossau', 32),
-(57, 'Maspistan', 32),
-(58, 'Aflary', 32),
-(59, 'Whairrusk District', 33),
-(60, 'Wrelbemp District', 33);
+INSERT INTO `jenny_fernandez` (`id`, `date`, `loan_amount`, `payment_amount`, `or_num`, `or_date`, `amount_balance`, `loan_balance`, `sc_starts`, `four_percent`, `sc_dates`, `months`, `four_percent_sc`, `sc_payments`, `sc_payments_or_num`, `sc_payments_date`, `sc_balance`) VALUES
+(2, '2023-09-02', 2100, 100, 21314, '2023-09-02', 2000, 0, '', 84, '', 0, 0, 0, 0, '0000-00-00', 0);
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `division`
+-- Table structure for table `lilibeth_cruz`
 --
 
-CREATE TABLE `division` (
-  `division_id` int(11) NOT NULL,
-  `division_name` varchar(255) NOT NULL,
-  `district_id` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `division`
---
-
-INSERT INTO `division` (`division_id`, `division_name`, `district_id`) VALUES
-(100, 'Itilima', 50),
-(102, 'Kinang\'weli', 50),
-(104, 'Brouc Skium', 56),
-(105, 'Gruip Thos', 56),
-(106, 'Zoshor Div', 57),
-(107, 'Cloaque Div', 58),
-(108, 'Smaules Div', 57),
-(109, 'Staggol Division', 59),
-(110, 'Bommek Division', 59),
-(111, 'Woweg Division', 60),
-(112, 'Spripret Division', 60);
-
--- --------------------------------------------------------
-
---
--- Table structure for table `groups`
---
-
-CREATE TABLE `groups` (
-  `group_id` int(11) NOT NULL,
-  `group_name` char(50) NOT NULL,
-  `region` varchar(50) NOT NULL,
-  `district` varchar(50) NOT NULL,
-  `division` varchar(50) NOT NULL,
-  `ward` varchar(50) NOT NULL,
-  `village` varchar(50) NOT NULL,
-  `registration_number` varchar(50) NOT NULL,
-  `group_activity` varchar(50) NOT NULL,
-  `group_category` varchar(50) NOT NULL,
-  `group_total_members` varchar(50) NOT NULL,
-  `group_leader` varchar(50) NOT NULL,
-  `loan_information` text NOT NULL,
-  `group_capital` varchar(100) NOT NULL,
-  `group_creation_date` date NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `groups`
---
-
-INSERT INTO `groups` (`group_id`, `group_name`, `region`, `district`, `division`, `ward`, `village`, `registration_number`, `group_activity`, `group_category`, `group_total_members`, `group_leader`, `loan_information`, `group_capital`, `group_creation_date`) VALUES
-(5, 'Astro Group', '32', '57', '106', '127', '234', 'CA101145', 'Activity 2', 'Group of Youth only', '7', 'Liam Moore', 'Demo Information', '250000', '2022-08-27'),
-(6, 'Shark Dynamos', '33', '60', '112', '138', '255', 'CA101234', 'Activity 4', 'Group of both Men and Women', '11', 'Betty Cooper', 'Sample Loan Info', '11550', '2022-08-27'),
-(7, 'Team Frog', '32', '57', '106', '129', '236', 'CA101699', 'Activity 3', 'Group of Women only', '3', 'Nathalie Hope', 'Demo Loan Info', '9900', '2022-08-27'),
-(8, 'Plexus Group', '32', '57', '106', '129', '235', 'CA104470', 'Activity 1', 'Group of Women only', '6', 'Marie Lopez', 'Test Test', '95000', '2022-08-27'),
-(9, 'Digi Group', '33', '60', '112', '138', '255', 'CA107969', 'Activity 3', 'Group of Youth only', '7', 'Curtis Johns', 'Test Info', '100000', '2022-08-27'),
-(10, 'Ether Group', '33', '60', '111', '136', '249', 'CA105557', 'Activity 3', 'Group of both Men and Women', '9', 'Christine Moore', 'Demo Loan Info', '99500', '2022-08-27');
+CREATE TABLE `lilibeth_cruz` (
+  `id` int(11) NOT NULL,
+  `date` date DEFAULT NULL,
+  `loan_amount` float DEFAULT NULL,
+  `payment_amount` float DEFAULT NULL,
+  `or_num` int(11) DEFAULT NULL,
+  `or_date` date DEFAULT NULL,
+  `amount_balance` float DEFAULT NULL,
+  `loan_balance` float DEFAULT NULL,
+  `sc_starts` varchar(255) DEFAULT NULL,
+  `four_percent` float DEFAULT NULL,
+  `sc_dates` varchar(255) DEFAULT NULL,
+  `months` int(11) DEFAULT NULL,
+  `four_percent_sc` float DEFAULT NULL,
+  `sc_payments` float DEFAULT NULL,
+  `sc_payments_or_num` int(11) DEFAULT NULL,
+  `sc_payments_date` date DEFAULT NULL,
+  `sc_balance` float DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
@@ -189,219 +164,61 @@ INSERT INTO `loan` (`loan_id`, `group_id`, `loan_come_from`, `loan_amount`, `loa
 
 CREATE TABLE `member` (
   `member_id` int(11) NOT NULL,
-  `first_name` char(50) NOT NULL,
-  `last_name` char(50) NOT NULL,
-  `gender` enum('m','f') NOT NULL,
-  `group_id` int(11) NOT NULL,
-  `join_date` date NOT NULL
+  `name` char(50) NOT NULL,
+  `unp-gl` float DEFAULT NULL,
+  `c-gl` float DEFAULT NULL,
+  `s/c` float DEFAULT NULL,
+  `amount` float DEFAULT NULL,
+  `or#` int(11) DEFAULT NULL,
+  `date` date DEFAULT NULL,
+  `remarks` varchar(500) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `member`
 --
 
-INSERT INTO `member` (`member_id`, `first_name`, `last_name`, `gender`, `group_id`, `join_date`) VALUES
-(1, 'Fred', 'Hutchens', 'm', 5, '2016-08-11'),
-(13, 'Thomas', 'Selvidge', 'm', 5, '2022-08-27'),
-(14, 'Darrel', 'Rutland', 'm', 5, '2022-08-27'),
-(15, 'Leslie', 'Stearns', 'f', 5, '2022-08-27'),
-(16, 'Clara', 'Feeney', 'f', 5, '2022-08-27'),
-(17, 'Betty', 'Cooper', 'f', 6, '2022-08-27'),
-(18, 'Emma', 'Merida', 'f', 6, '2022-08-27'),
-(19, 'John', 'Wilson', 'm', 6, '2022-08-27'),
-(20, 'Michael', 'Schwartz', 'm', 6, '2022-08-27'),
-(21, 'Gerald', 'Walston', 'm', 6, '2022-08-27'),
-(22, 'Stephanie', 'Grosse', 'f', 6, '2022-08-27'),
-(23, 'Cynthia', 'Sites', 'f', 5, '2022-08-27'),
-(24, 'Stephen', 'Martinez', 'm', 6, '2022-08-27'),
-(25, 'Liam', 'Moore', 'm', 5, '2022-08-27'),
-(26, 'Nathalie', 'Hope', 'f', 7, '2022-08-27'),
-(27, 'Henry', 'Doe', 'm', 7, '2022-08-27'),
-(28, 'Marie', 'Lopez', 'f', 8, '2022-08-27'),
-(29, 'Kathryn', 'Cruiz', 'f', 8, '2022-08-27'),
-(30, 'Nancy', 'Jane', 'f', 8, '2022-08-27'),
-(31, 'Elton', 'Simmons', 'm', 6, '2022-08-27'),
-(32, 'Andrew', 'Rosinski', 'm', 7, '2022-08-27'),
-(33, 'Curtis', 'Johns', 'm', 9, '2022-08-27'),
-(34, 'Rebecca', 'Vaughn', 'f', 9, '2022-08-27'),
-(35, 'Helen', 'Reardon', 'f', 9, '2022-08-27'),
-(36, 'Joseph', 'Murphy', 'm', 9, '2022-08-27'),
-(37, 'Christine', 'Moore', 'f', 10, '2022-08-27'),
-(38, 'Kate', 'Lenz', 'f', 10, '2022-08-27');
+INSERT INTO `member` (`member_id`, `name`, `unp-gl`, `c-gl`, `s/c`, `amount`, `or#`, `date`, `remarks`) VALUES
+(77, 'Jenny Fernandez', 1000, 0, 0, 0, 2147483647, '2023-09-21', '0'),
+(80, 'Lilibeth Cruz', 236000, 40000, 20000, 23456, 56756754, '2023-09-06', '0');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `payment_history`
+-- Table structure for table `sales_collection_summary`
 --
 
-CREATE TABLE `payment_history` (
-  `payment_id` int(11) NOT NULL,
-  `group_id` int(11) NOT NULL,
-  `amount_paid` int(11) NOT NULL,
-  `payment_date` date NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+CREATE TABLE `sales_collection_summary` (
+  `id` int(11) NOT NULL,
+  `table_name` varchar(500) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data for table `payment_history`
+-- Dumping data for table `sales_collection_summary`
 --
 
-INSERT INTO `payment_history` (`payment_id`, `group_id`, `amount_paid`, `payment_date`) VALUES
-(3, 5, 9815, '2022-08-27'),
-(4, 7, 9030, '2022-08-27'),
-(5, 10, 2750, '2022-08-27');
+INSERT INTO `sales_collection_summary` (`id`, `table_name`) VALUES
+(3, 'Cash Collection 2020'),
+(4, 'sample table');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `region`
+-- Table structure for table `sample_table`
 --
 
-CREATE TABLE `region` (
-  `region_id` int(11) NOT NULL,
-  `region_name` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `region`
---
-
-INSERT INTO `region` (`region_id`, `region_name`) VALUES
-(25, 'Region One'),
-(32, 'Region Two'),
-(33, 'Region Three');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `village`
---
-
-CREATE TABLE `village` (
-  `village_id` int(11) NOT NULL,
-  `village_name` varchar(255) NOT NULL,
-  `ward_id` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `village`
---
-
-INSERT INTO `village` (`village_id`, `village_name`, `ward_id`) VALUES
-(156, 'Mwalushu', 111),
-(157, 'Mwamigagani', 111),
-(158, 'Mwamanyangu', 111),
-(159, 'Mwanunui', 111),
-(160, 'Ng\'homango', 111),
-(161, 'Ntagaswa', 111),
-(162, 'Mwamapalala', 112),
-(163, 'Nkololo Itilima', 112),
-(164, 'Ngeme', 112),
-(165, 'Mwamunhu', 112),
-(166, 'Idoselo', 112),
-(167, 'Nkoma', 113),
-(168, 'Dasina', 113),
-(169, 'Gambasingu', 113),
-(170, 'Ng\'wang\'wita', 113),
-(171, 'Musoma', 113),
-(172, 'Mwambagwa', 113),
-(173, 'Nyamalapa', 114),
-(174, 'Kimali', 114),
-(175, 'Itilima', 114),
-(176, 'B/Mbugani', 114),
-(177, 'Isakang\'hwale', 114),
-(204, 'Mwakilangi', 121),
-(205, 'Isengwa', 121),
-(206, 'Kinang\'weli', 121),
-(207, 'Mwagimagi', 121),
-(208, 'Lalang\'ombe', 121),
-(209, 'Luguru', 122),
-(210, 'Ikungulipu', 122),
-(211, 'Itubilo', 122),
-(212, 'Inalo', 122),
-(213, 'Kidula', 123),
-(214, 'Sunzula', 123),
-(215, 'Ng\'hesha Itilima', 123),
-(216, 'Mwamnemha', 123),
-(217, 'Nhobora', 124),
-(218, 'Mwabuyunge', 124),
-(219, 'Mwaumisheni', 124),
-(220, 'Tagawi', 124),
-(221, 'Kilugala', 124),
-(222, 'Sawida', 125),
-(223, 'Isagala', 125),
-(224, 'Mahembe', 125),
-(225, 'Songambele', 125),
-(226, 'Bukingwamizi', 126),
-(227, 'Zanzui', 126),
-(228, 'Mlimani', 126),
-(229, 'Sasago', 126),
-(230, 'Kabale', 126),
-(231, 'Kashishi', 126),
-(232, 'Aflurg Village', 127),
-(233, 'Aflurg Village', 128),
-(234, 'Espar Village', 127),
-(235, 'Troy Village', 129),
-(236, 'Desp Village', 129),
-(237, 'Neey Village', 130),
-(238, 'QWER Village', 130),
-(239, 'Modok Village', 131),
-(240, 'Western Bid Village', 131),
-(241, 'Beyside Village', 132),
-(242, 'Xuggen L Village', 132),
-(243, 'DXC Village', 133),
-(244, 'Getteamlift Village', 133),
-(245, 'Western Bloe Village', 134),
-(246, 'Eastern WTR Village', 134),
-(247, 'Droogoston Village', 135),
-(248, 'Dracary Village', 135),
-(249, 'SW Village', 136),
-(250, 'NS Village', 136),
-(251, 'SNP Village', 137),
-(252, 'SNP Village', 137),
-(253, 'Deweect North Village', 137),
-(254, 'Skeganlic Village', 138),
-(255, 'Sphek Cross Village', 138);
-
--- --------------------------------------------------------
-
---
--- Table structure for table `ward`
---
-
-CREATE TABLE `ward` (
-  `ward_id` int(11) NOT NULL,
-  `ward_name` varchar(255) NOT NULL,
-  `division_id` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `ward`
---
-
-INSERT INTO `ward` (`ward_id`, `ward_name`, `division_id`) VALUES
-(111, 'Mwalushu', 100),
-(112, 'Mwamapalala', 100),
-(113, 'Nkoma', 100),
-(114, 'Nyamalapa', 100),
-(121, 'Kinang\'weli', 102),
-(122, 'Luguru', 102),
-(123, 'Mbita', 102),
-(124, 'Nhobora', 102),
-(125, 'Sawida', 102),
-(126, 'Zagayu', 102),
-(127, 'Oplil Ward', 106),
-(128, 'Oplil Ward', 107),
-(129, 'Ecla Ward', 106),
-(130, 'Stad Ward', 106),
-(131, 'Reeot Ward', 109),
-(132, 'Peggot Ward One', 109),
-(133, 'Stareb Ward', 110),
-(134, 'Myclone Ward T', 110),
-(135, 'Stareb Ward', 111),
-(136, 'Joccark Ward', 111),
-(137, 'Taffeost Ward', 112),
-(138, 'Stareb Ward', 112);
+CREATE TABLE `sample_table` (
+  `id` int(11) NOT NULL,
+  `date` date DEFAULT NULL,
+  `or_num` int(11) DEFAULT NULL,
+  `charged_colln` float DEFAULT NULL,
+  `charged_total` float DEFAULT NULL,
+  `d_sales_for` float DEFAULT NULL,
+  `amount` float DEFAULT NULL,
+  `overage` float DEFAULT NULL,
+  `total` float DEFAULT NULL,
+  `c_total` float DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Indexes for dumped tables
@@ -414,28 +231,22 @@ ALTER TABLE `admin`
   ADD PRIMARY KEY (`admin_id`);
 
 --
--- Indexes for table `category`
+-- Indexes for table `cash_collection_2020`
 --
-ALTER TABLE `category`
-  ADD PRIMARY KEY (`cat_id`);
+ALTER TABLE `cash_collection_2020`
+  ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `district`
+-- Indexes for table `jenny_fernandez`
 --
-ALTER TABLE `district`
-  ADD PRIMARY KEY (`district_id`);
+ALTER TABLE `jenny_fernandez`
+  ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `division`
+-- Indexes for table `lilibeth_cruz`
 --
-ALTER TABLE `division`
-  ADD PRIMARY KEY (`division_id`);
-
---
--- Indexes for table `groups`
---
-ALTER TABLE `groups`
-  ADD PRIMARY KEY (`group_id`);
+ALTER TABLE `lilibeth_cruz`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `loan`
@@ -450,28 +261,16 @@ ALTER TABLE `member`
   ADD PRIMARY KEY (`member_id`);
 
 --
--- Indexes for table `payment_history`
+-- Indexes for table `sales_collection_summary`
 --
-ALTER TABLE `payment_history`
-  ADD PRIMARY KEY (`payment_id`);
+ALTER TABLE `sales_collection_summary`
+  ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `region`
+-- Indexes for table `sample_table`
 --
-ALTER TABLE `region`
-  ADD PRIMARY KEY (`region_id`);
-
---
--- Indexes for table `village`
---
-ALTER TABLE `village`
-  ADD PRIMARY KEY (`village_id`);
-
---
--- Indexes for table `ward`
---
-ALTER TABLE `ward`
-  ADD PRIMARY KEY (`ward_id`);
+ALTER TABLE `sample_table`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -482,26 +281,50 @@ ALTER TABLE `ward`
 --
 ALTER TABLE `admin`
   MODIFY `admin_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
 --
--- AUTO_INCREMENT for table `groups`
+-- AUTO_INCREMENT for table `cash_collection_2020`
 --
-ALTER TABLE `groups`
-  MODIFY `group_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+ALTER TABLE `cash_collection_2020`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `jenny_fernandez`
+--
+ALTER TABLE `jenny_fernandez`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+
+--
+-- AUTO_INCREMENT for table `lilibeth_cruz`
+--
+ALTER TABLE `lilibeth_cruz`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
 --
 -- AUTO_INCREMENT for table `loan`
 --
 ALTER TABLE `loan`
   MODIFY `loan_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
 --
 -- AUTO_INCREMENT for table `member`
 --
 ALTER TABLE `member`
-  MODIFY `member_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=39;
+  MODIFY `member_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=81;
+
 --
--- AUTO_INCREMENT for table `payment_history`
+-- AUTO_INCREMENT for table `sales_collection_summary`
 --
-ALTER TABLE `payment_history`
-  MODIFY `payment_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+ALTER TABLE `sales_collection_summary`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `sample_table`
+--
+ALTER TABLE `sample_table`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
